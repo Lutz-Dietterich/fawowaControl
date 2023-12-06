c642ee8129ca00d16d20cdbfc464ba369b927fba
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-12-06 21:15:05 +0100

    refactor main.py

--- main.py
+++ main.py
@@ -4,27 +4,21 @@ import machine
 import os 
 import config
 
+# Funktion zum Herstellen der WLAN-Verbindung
+def connect_to_wifi(ssid, password):
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    station.connect(ssid, password)
 
-ssid = config.SSID
-password = config.PASSWORT
+    while not station.isconnected():
+        pass
 
-station = network.WLAN(network.STA_IF)
+    print('Verbindung hergestellt')
+    print(station.ifconfig())
 
-station.active(True)
-station.connect(ssid, password)
-
-while station.isconnected() == False:
-  pass
-
-print('Verbindung hergestellt')
-print(station.ifconfig())
-
-# Konfigurieren Sie einen GPIO-Pin
-pin = machine.Pin(2, machine.Pin.OUT)
-
+# Funktion zum Lesen von Dateien
 def read_file(name):
     try:
-        # Bestimmen des Content-Types basierend auf der Dateiendung
         if name.endswith(".html"):
             content_type = "text/html"
             mode = 'r'
@@ -41,16 +35,27 @@ def read_file(name):
             content_type = "text/plain"
             mode = 'r'
 
-        # Öffnen der Datei im entsprechenden Modus
         with open(name, mode) as file:
             return True, content_type, file.read()
     except:
         return False, "text/plain", "404 Not Found"
 
+# Funktion zur Steuerung der LED
+def control_led(pin, state):
+    if state == "on":
+        pin.on()
+        return "LED ist an"
+    elif state == "off":
+        pin.off()
+        return "LED ist aus"
 
+# Funktion zum Senden von HTTP-Antworten
+def send_http_response(client, status_code, content_type, content):
+    response = f'HTTP/1.0 {status_code} OK\r\nContent-type: {content_type}\r\n\r\n{content}'
+    client.send(response.encode('utf-8'))
 
-
-def start_server():
+# Hauptfunktion zum Starten des Webservers
+def start_server(pin):
     addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
     s = socket.socket()
     s.bind(addr)
@@ -71,33 +76,30 @@ def start_server():
             parts = request_line.split(" ")
             if len(parts) > 1:
                 filename = parts[1]
-                
-                if filename == "/on":
-                    pin.on()
-                    response = "LED ist an"
-                    cl.send('HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n{}'.format(response).encode('utf-8'))
-                    
-                elif filename == "/off":
-                    pin.off()
-                    response = "LED ist aus"
-                    cl.send('HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n{}'.format(response).encode('utf-8'))
-                    
+
+                if filename == "/on" or filename == "/off":
+                    response = control_led(pin, filename[1:])
+                    send_http_response(cl, 200, "text/plain", response)
                 else:
                     if filename == "/":
                         filename = "/index.html"
-
                     found, content_type, response = read_file('www' + filename)
                     if found:
-                        cl.send('HTTP/1.0 200 OK\r\nContent-type: {}\r\n\r\n{}'.format(content_type, response).encode('utf-8'))
+                        send_http_response(cl, 200, content_type, response)
                     else:
-                        cl.send('HTTP/1.0 404 Not Found\r\nContent-type: text/html\r\n\r\n404 Not Found'.encode('utf-8'))
-                        
+                        send_http_response(cl, 404, "text/html", "404 Not Found")
             else:
-                cl.send('HTTP/1.0 400 Bad Request\r\nContent-type: text/html\r\n\r\n400 Bad Request'.encode('utf-8'))
-                
+                send_http_response(cl, 400, "text/html", "400 Bad Request")
         else:
-            cl.send('HTTP/1.0 400 Bad Request\r\nContent-type: text/html\r\n\r\n400 Bad Request'.encode('utf-8'))
-                
+            send_http_response(cl, 400, "text/html", "400 Bad Request")
+
         cl.close()
-            
-start_server()
+
+# Verbindung zum WLAN herstellen
+connect_to_wifi(config.SSID, config.PASSWORD)
+
+# Konfigurieren eines GPIO-Pins
+pin = machine.Pin(2, machine.Pin.OUT)
+
+# Starten des Webservers
+start_server(pin)
