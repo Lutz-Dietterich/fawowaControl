5fdaaa5d41584782d28cc9211c8594fec53521d8
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-12-11 22:27:09 +0100

    add navi.ts implement switch to active menu buttons and toggle active color

--- typescript/navi.ts
+++ typescript/navi.ts
@@ -1,54 +1,80 @@
-
-
-    interface DataJsElement extends HTMLElement {
-        dataset: {
-            js: string;
-        };
-    }
     
     const settingsOn = document.querySelector('[data-js="icon__settings--off"]') as SVGSVGElement | null;
     const settingsOff = document.querySelector('[data-js="icon__settings--on"]') as SVGSVGElement | null;
     const settingsButton = document.querySelector('[data-js="footer__button--settings"]') as HTMLButtonElement | null;
+    let settings: boolean = false;
+
     const homeOn = document.querySelector('[data-js="icon__home--off"]') as SVGSVGElement | null;
     const homeOff = document.querySelector('[data-js="icon__home--on"]') as SVGSVGElement | null;
     const homeButton = document.querySelector('[data-js="footer__button--home"]') as HTMLButtonElement | null;
-    const bulbOn = document.querySelector('[data-js="icon__bulb--off"]');
-    const bulbOff = document.querySelector('[data-js="icon__bulb--on"]');
-    const bulbButton = document.querySelector('[data-js="footer__button--bulb"]');
+    let home: boolean = true;
+
+    const lightOn = document.querySelector('[data-js="icon__light--off"]') as SVGSVGElement | null;
+    const lightOff = document.querySelector('[data-js="icon__light--on"]') as SVGSVGElement | null;
+    const lightButton = document.querySelector('[data-js="footer__button--light"]') as HTMLButtonElement | null;
+    let light: boolean = false;
     
 
 
-    // Überprüfen, ob alle Elemente vorhanden sind
-    if (settingsOff && settingsOn && settingsButton) {
-        // Umschalten der Sichtbarkeit der SVGs beim Klicken auf den Button
+    if (settingsOff && settingsOn && settingsButton && homeOff && homeOn && homeButton && lightOff && lightOn && lightButton) {
+        
         settingsButton.addEventListener("click", () => {
+            
+            if (!settings) {
+            settings = !settings;
             settingsOff.classList.toggle('visually-hidden');
             settingsOn.classList.toggle('visually-hidden');
-        });
-    } else {
-        console.error("Eines oder mehrere erforderliche Elemente fehlen im DOM.");
-    }
-    
-
-    if (homeOff && homeOn && homeButton) {
-        // Umschalten der Sichtbarkeit der SVGs beim Klicken auf den Button
-        homeButton.addEventListener("click", () => {
+            }
+            if (home) {
+            home = !home;
             homeOff.classList.toggle('visually-hidden');
             homeOn.classList.toggle('visually-hidden');
+            }
+            if (light) {
+            light = !light;
+            lightOff.classList.toggle('visually-hidden');
+            lightOn.classList.toggle('visually-hidden');
+            }
         });
+    
+        homeButton.addEventListener("click", () => {
+
+            if (settings) {
+                settings = !settings;
+                settingsOff.classList.toggle('visually-hidden');
+                settingsOn.classList.toggle('visually-hidden');
+                }
+                if (!home) {
+                home = !home;
+                homeOff.classList.toggle('visually-hidden');
+                homeOn.classList.toggle('visually-hidden');
+                }
+                if (light) {
+                light = !light;
+                lightOff.classList.toggle('visually-hidden');
+                lightOn.classList.toggle('visually-hidden');
+                }
+        });
+
+        lightButton.addEventListener("click", () => {
+
+            if (settings) {
+                settings = !settings;
+                settingsOff.classList.toggle('visually-hidden');
+                settingsOn.classList.toggle('visually-hidden');
+                }
+                if (home) {
+                home = !home;
+                homeOff.classList.toggle('visually-hidden');
+                homeOn.classList.toggle('visually-hidden');
+                }
+                if (!light) {
+                light = !light;
+                lightOff.classList.toggle('visually-hidden');
+                lightOn.classList.toggle('visually-hidden');
+                }
+        });
+
     } else {
-        console.error("Eines oder mehrere erforderliche Elemente fehlen im DOM.");
-    }
-
-
-
-    if (bulbOff && bulbOn && bulbButton) {
-        // Umschalten der Sichtbarkeit der SVGs beim Klicken auf den Button
-        bulbButton.addEventListener("click", () => {
-        bulbOff.classList.toggle('visually-hidden');
-            bulbOn.classList.toggle('visually-hidden');
-        });
-    }
-    else {
         console.error("Eines oder mehrere erforderliche Elemente fehlen im DOM.");
     }
--- www/js/navi.js
+++ www/js/navi.js
@@ -2,38 +2,66 @@
 const settingsOn = document.querySelector('[data-js="icon__settings--off"]');
 const settingsOff = document.querySelector('[data-js="icon__settings--on"]');
 const settingsButton = document.querySelector('[data-js="footer__button--settings"]');
+let settings = false;
 const homeOn = document.querySelector('[data-js="icon__home--off"]');
 const homeOff = document.querySelector('[data-js="icon__home--on"]');
 const homeButton = document.querySelector('[data-js="footer__button--home"]');
-const bulbOn = document.querySelector('[data-js="icon__bulb--off"]');
-const bulbOff = document.querySelector('[data-js="icon__bulb--on"]');
-const bulbButton = document.querySelector('[data-js="footer__button--bulb"]');
-// Überprüfen, ob alle Elemente vorhanden sind
-if (settingsOff && settingsOn && settingsButton) {
-    // Umschalten der Sichtbarkeit der SVGs beim Klicken auf den Button
+let home = true;
+const lightOn = document.querySelector('[data-js="icon__light--off"]');
+const lightOff = document.querySelector('[data-js="icon__light--on"]');
+const lightButton = document.querySelector('[data-js="footer__button--light"]');
+let light = false;
+if (settingsOff && settingsOn && settingsButton && homeOff && homeOn && homeButton && lightOff && lightOn && lightButton) {
     settingsButton.addEventListener("click", () => {
-        settingsOff.classList.toggle('visually-hidden');
-        settingsOn.classList.toggle('visually-hidden');
+        if (!settings) {
+            settings = !settings;
+            settingsOff.classList.toggle('visually-hidden');
+            settingsOn.classList.toggle('visually-hidden');
+        }
+        if (home) {
+            home = !home;
+            homeOff.classList.toggle('visually-hidden');
+            homeOn.classList.toggle('visually-hidden');
+        }
+        if (light) {
+            light = !light;
+            lightOff.classList.toggle('visually-hidden');
+            lightOn.classList.toggle('visually-hidden');
+        }
     });
-}
-else {
-    console.error("Eines oder mehrere erforderliche Elemente fehlen im DOM.");
-}
-if (homeOff && homeOn && homeButton) {
-    // Umschalten der Sichtbarkeit der SVGs beim Klicken auf den Button
     homeButton.addEventListener("click", () => {
-        homeOff.classList.toggle('visually-hidden');
-        homeOn.classList.toggle('visually-hidden');
+        if (settings) {
+            settings = !settings;
+            settingsOff.classList.toggle('visually-hidden');
+            settingsOn.classList.toggle('visually-hidden');
+        }
+        if (!home) {
+            home = !home;
+            homeOff.classList.toggle('visually-hidden');
+            homeOn.classList.toggle('visually-hidden');
+        }
+        if (light) {
+            light = !light;
+            lightOff.classList.toggle('visually-hidden');
+            lightOn.classList.toggle('visually-hidden');
+        }
     });
-}
-else {
-    console.error("Eines oder mehrere erforderliche Elemente fehlen im DOM.");
-}
-if (bulbOff && bulbOn && bulbButton) {
-    // Umschalten der Sichtbarkeit der SVGs beim Klicken auf den Button
-    bulbButton.addEventListener("click", () => {
-        bulbOff.classList.toggle('visually-hidden');
-        bulbOn.classList.toggle('visually-hidden');
+    lightButton.addEventListener("click", () => {
+        if (settings) {
+            settings = !settings;
+            settingsOff.classList.toggle('visually-hidden');
+            settingsOn.classList.toggle('visually-hidden');
+        }
+        if (home) {
+            home = !home;
+            homeOff.classList.toggle('visually-hidden');
+            homeOn.classList.toggle('visually-hidden');
+        }
+        if (!light) {
+            light = !light;
+            lightOff.classList.toggle('visually-hidden');
+            lightOn.classList.toggle('visually-hidden');
+        }
     });
 }
 else {
