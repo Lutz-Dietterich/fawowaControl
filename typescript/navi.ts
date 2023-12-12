7c3107b6dbb379fa947b3e4cdfa3a7e50c966ee4
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-12-12 22:51:40 +0100

    create card switching

--- typescript/navi.ts
+++ typescript/navi.ts
@@ -3,23 +3,57 @@
     const settingsOn = document.querySelector('[data-js="icon__settings--off"]') as SVGSVGElement | null;
     const settingsOff = document.querySelector('[data-js="icon__settings--on"]') as SVGSVGElement | null;
     const settingsButton = document.querySelector('[data-js="footer__button--settings"]') as HTMLButtonElement | null;
+    const settingsCardTemp = document.querySelector('[data-js="card__settings--temp"]') as HTMLElement | null;
 
     let home: boolean = true;
     const homeOn = document.querySelector('[data-js="icon__home--off"]') as SVGSVGElement | null;
     const homeOff = document.querySelector('[data-js="icon__home--on"]') as SVGSVGElement | null;
     const homeButton = document.querySelector('[data-js="footer__button--home"]') as HTMLButtonElement | null;
+    const homeCard = document.querySelector('[data-js="card__home"]') as HTMLElement | null;
 
     let light: boolean = false;
     const lightOn = document.querySelector('[data-js="icon__light--off"]') as SVGSVGElement | null;
     const lightOff = document.querySelector('[data-js="icon__light--on"]') as SVGSVGElement | null;
     const lightButton = document.querySelector('[data-js="footer__button--light"]') as HTMLButtonElement | null;
+    const lightCard = document.querySelector('[data-js="card__light"]') as HTMLElement | null;
     
 
 
-    if (settingsOff && settingsOn && settingsButton && homeOff && homeOn && homeButton && lightOff && lightOn && lightButton) {
+    const updateVisibility = (): void => {
+        console.log('updateVisibility');
+    
+        if (settingsCardTemp) {
+            if (settings) {
+                settingsCardTemp.classList.remove('visually-hidden');
+            } 
+            if (!settings) {
+                settingsCardTemp.classList.add('visually-hidden');
+            }
+        }
+    
+        if (homeCard) {
+            if (home) {
+                homeCard.classList.remove('visually-hidden');
+            } 
+            if (!home) {
+                homeCard.classList.add('visually-hidden');
+            }
+        }
+    
+        if (lightCard) {
+            if (light) {
+                lightCard.classList.remove('visually-hidden');
+            } 
+            if (!light) {
+                lightCard.classList.add('visually-hidden');
+            }
+        }
+    };
+    
+
+    if (settingsOff && settingsOn && settingsButton && homeOff && homeOn && homeButton && lightOff && lightOn && lightButton && settingsCardTemp && homeCard && lightCard) {
         
         settingsButton.addEventListener("click", () => {
-            
             if (!settings) {
             settings = !settings;
             settingsOff.classList.toggle('visually-hidden');
@@ -35,44 +69,45 @@
             lightOff.classList.toggle('visually-hidden');
             lightOn.classList.toggle('visually-hidden');
             }
+            updateVisibility();
         });
     
         homeButton.addEventListener("click", () => {
-
             if (settings) {
-                settings = !settings;
-                settingsOff.classList.toggle('visually-hidden');
-                settingsOn.classList.toggle('visually-hidden');
-                }
-                if (!home) {
-                home = !home;
-                homeOff.classList.toggle('visually-hidden');
-                homeOn.classList.toggle('visually-hidden');
-                }
-                if (light) {
-                light = !light;
-                lightOff.classList.toggle('visually-hidden');
-                lightOn.classList.toggle('visually-hidden');
-                }
+            settings = !settings;
+            settingsOff.classList.toggle('visually-hidden');
+            settingsOn.classList.toggle('visually-hidden');
+            }
+            if (!home) {
+            home = !home;
+            homeOff.classList.toggle('visually-hidden');
+            homeOn.classList.toggle('visually-hidden');
+            }
+            if (light) {
+            light = !light;
+            lightOff.classList.toggle('visually-hidden');
+            lightOn.classList.toggle('visually-hidden');
+            }
+            updateVisibility();
         });
 
         lightButton.addEventListener("click", () => {
-
             if (settings) {
-                settings = !settings;
-                settingsOff.classList.toggle('visually-hidden');
-                settingsOn.classList.toggle('visually-hidden');
-                }
-                if (home) {
-                home = !home;
-                homeOff.classList.toggle('visually-hidden');
-                homeOn.classList.toggle('visually-hidden');
-                }
-                if (!light) {
-                light = !light;
-                lightOff.classList.toggle('visually-hidden');
-                lightOn.classList.toggle('visually-hidden');
-                }
+            settings = !settings;
+            settingsOff.classList.toggle('visually-hidden');
+            settingsOn.classList.toggle('visually-hidden');
+            }
+            if (home) {
+            home = !home;
+            homeOff.classList.toggle('visually-hidden');
+            homeOn.classList.toggle('visually-hidden');
+            }
+            if (!light) {
+            light = !light;
+            lightOff.classList.toggle('visually-hidden');
+            lightOn.classList.toggle('visually-hidden');
+            }
+            updateVisibility();
         });
 
     } else {
--- www/js/navi.js
+++ www/js/navi.js
@@ -1,18 +1,51 @@
 "use strict";
+let settings = false;
 const settingsOn = document.querySelector('[data-js="icon__settings--off"]');
 const settingsOff = document.querySelector('[data-js="icon__settings--on"]');
 const settingsButton = document.querySelector('[data-js="footer__button--settings"]');
-let settings = false;
+const settingsCardTemp = document.querySelector('[data-js="card__settings--temp"]');
+let home = true;
 const homeOn = document.querySelector('[data-js="icon__home--off"]');
 const homeOff = document.querySelector('[data-js="icon__home--on"]');
 const homeButton = document.querySelector('[data-js="footer__button--home"]');
-let home = true;
+const homeCard = document.querySelector('[data-js="card__home"]');
+let light = false;
 const lightOn = document.querySelector('[data-js="icon__light--off"]');
 const lightOff = document.querySelector('[data-js="icon__light--on"]');
 const lightButton = document.querySelector('[data-js="footer__button--light"]');
-let light = false;
-if (settingsOff && settingsOn && settingsButton && homeOff && homeOn && homeButton && lightOff && lightOn && lightButton) {
+const lightCard = document.querySelector('[data-js="card__light"]');
+const updateVisibility = () => {
+    setTimeout(() => {
+        console.log('updateVisibility');
+        if (settingsCardTemp) {
+            if (settings) {
+                settingsCardTemp.classList.remove('visually-hidden');
+            }
+            if (!settings) {
+                settingsCardTemp.classList.add('visually-hidden');
+            }
+        }
+        if (homeCard) {
+            if (home) {
+                homeCard.classList.remove('visually-hidden');
+            }
+            if (!home) {
+                homeCard.classList.add('visually-hidden');
+            }
+        }
+        if (lightCard) {
+            if (light) {
+                lightCard.classList.remove('visually-hidden');
+            }
+            if (!light) {
+                lightCard.classList.add('visually-hidden');
+            }
+        }
+    }, 500);
+};
+if (settingsOff && settingsOn && settingsButton && homeOff && homeOn && homeButton && lightOff && lightOn && lightButton && settingsCardTemp && homeCard && lightCard) {
     settingsButton.addEventListener("click", () => {
+        updateVisibility();
         if (!settings) {
             settings = !settings;
             settingsOff.classList.toggle('visually-hidden');
@@ -30,6 +63,7 @@ if (settingsOff && settingsOn && settingsButton && homeOff && homeOn && homeButt
         }
     });
     homeButton.addEventListener("click", () => {
+        updateVisibility();
         if (settings) {
             settings = !settings;
             settingsOff.classList.toggle('visually-hidden');
@@ -47,6 +81,7 @@ if (settingsOff && settingsOn && settingsButton && homeOff && homeOn && homeButt
         }
     });
     lightButton.addEventListener("click", () => {
+        updateVisibility();
         if (settings) {
             settings = !settings;
             settingsOff.classList.toggle('visually-hidden');
