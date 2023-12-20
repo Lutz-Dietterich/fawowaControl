b785aedbcfdb25511b39274e900f69e452bc984a
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-12-21 00:23:44 +0100

    rename gauge to setpoint + styling setting card

--- typescript/setpoint.ts
+++ typescript/setpoint.ts
@@ -1,26 +1,26 @@
 
-const currentValue = document.querySelector('[data-js="settings-card__gauge--current-value"]') as HTMLInputElement | null;
+const currentValue = document.querySelector('[data-js="settings-card__setpoint--current-value"]') as HTMLInputElement | null;
 const buttonMinus = document.querySelector('[data-js="settings-card__button-round--minus"]') as HTMLButtonElement | null;
 const buttonPlus = document.querySelector('[data-js="settings-card__button-round--plus"]') as HTMLButtonElement | null;
-const gaugeScale = document.querySelector('[data-js="settings-card__gauge--scale"]') as SVGCircleElement | null;
+const setpointScale = document.querySelector('[data-js="settings-card__setpoint--scale"]') as SVGCircleElement | null;
 
 document.addEventListener("DOMContentLoaded", () => {
     if (currentValue) {
         const startValue: number = 725 - (parseInt(currentValue.value, 10) / 35) * (725 - 170);
-        updateGauge(startValue - 5);
+        updatesetpoint(startValue - 5);
     }
 });
 
-function updateGauge(newValue: number) {
+function updatesetpoint(newValue: number) {
     let newOffset = 725 - (newValue / 35) * (725 - 170);
-    gaugeScale?.setAttribute("stroke-dashoffset", newOffset.toString());
+    setpointScale?.setAttribute("stroke-dashoffset", newOffset.toString());
 }
 if (currentValue) {
     buttonMinus?.addEventListener("click", () => {
         let newValue: number = parseInt(currentValue.value, 10);
         if (newValue > 0) {
             currentValue.value = (newValue - 1).toString();
-            updateGauge(newValue - 1);
+            updatesetpoint(newValue - 1);
         }
     });
 
@@ -28,7 +28,7 @@ if (currentValue) {
         let newValue = parseInt(currentValue.value, 10);
         if (newValue < 35) {
             currentValue.value = (newValue + 1).toString();
-            updateGauge(newValue + 1);
+            updatesetpoint(newValue + 1);
         }
     });
 };
